696aac065b20f10b993772d27532a06aab95fafc
KlaudiaNarekiewicz <62132418+KlaudiaNarekiewicz@users.noreply.github.com>
2020-06-18 22:51:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 bool isAnagram(string linia11, string linia22)
 {
     if(linia11.length() != linia22.length()) 
-	return false; 
-	for (int i=0; i<linia11.length(); i++) 
- 	{
-    	if (linia11[i] = linia22[i])
-		return true;
+    return false; 
+    for (int i=0; i<linia11.length(); i++) 
+    {
+     	if (linia11[i] = linia22[i])
+	return true;
     }
 }
 string stringSort(string s)
@@ -19,14 +19,14 @@ string stringSort(string s)
 	int dlugosc = napis.length();
 	for(int i=0; i<dlugosc-1; i++)
    	{
-        for(int j=0; j<dlugosc-1; j++)
-        {
-           if(napis[j]>napis[j+1])
-           {
-                char litera = napis[j];
-                napis[j] = napis[j+1];
-                napis[j+1] = litera;
-           }
+        	for(int j=0; j<dlugosc-1; j++)
+        	{
+           		if(napis[j]>napis[j+1])
+           		{
+                		char litera = napis[j];
+                		napis[j] = napis[j+1];
+                		napis[j+1] = litera;
+           		}
 		}
 	}
 return napis;
@@ -35,24 +35,23 @@ int main(int argc, char** argv)
 {
 	ifstream plik1;
 	ofstream plik2;
-        
+	
 	string linia1;
-    string linia2;
-
+        string linia2;
+	
 	string linia11;
-    string linia22;
-    
+        string linia22;
+	
 	plik1.open("1.txt");
-   	plik2.open("2.txt");
-   	
-	plik2<<"["<<endl;
-   		
-    while(!plik1.eof())
+   	plik2.open("2.txt");   	
+	plik2<<"["<<endl;  
+	
+        while(!plik1.eof())
 	{	
 		getline(plik1,linia1);  
 		for(int i=0;i<2;i++)
 		{
-    		getline(plik1,linia2);       
+    			getline(plik1,linia2);       
 			linia11 = stringSort(linia1);
 			linia22 = stringSort(linia2);        
    			if(isAnagram(linia11,linia22))
